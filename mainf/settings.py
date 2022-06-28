4fe8cd3aad2013925248bdd28805fe3b612b9676
JorgeAlvarez21 <joglnvz@gmail.com>
2022-06-27 19:12:31 -0500

    API functionality for Bookstore database and HTML template component

--- mainf/settings.py
+++ mainf/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'demo',
+    'rest_framework',
+    'App',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
